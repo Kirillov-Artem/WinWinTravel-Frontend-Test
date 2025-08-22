953a59495a5aeee0c7b823499eb8be5c41e78c52
Kirillov-Artem <artemkirillov2017@gmail.com>
2025-08-22 20:31:55 +0300

    fix: remove unused eslint directives from i18n config

--- src/shared/i18n/i18n.ts
+++ src/shared/i18n/i18n.ts
@@ -1,17 +1,18 @@
 import { initReactI18next } from 'react-i18next'
 
-import i18n from 'i18next'
+import i18next from 'i18next'
 
 import { I18N_DEFAULT_LANGUAGE, I18N_DEFAULT_NS } from './i18nConstants'
 import { resources } from './locales'
 
-i18n.use(initReactI18next).init({
-	resources: resources,
-	debug: true,
-	// Remove on connect language detector
-	lng: I18N_DEFAULT_LANGUAGE,
-	interpolation: {
-		escapeValue: false
-	},
-	defaultNS: I18N_DEFAULT_NS
-})
+;(i18next as any) /* eslint-disable-line @typescript-eslint/no-explicit-any */
+	.use(initReactI18next)
+	.init({
+		resources: resources,
+		debug: true,
+		lng: I18N_DEFAULT_LANGUAGE,
+		interpolation: {
+			escapeValue: false
+		},
+		defaultNS: I18N_DEFAULT_NS
+	})
