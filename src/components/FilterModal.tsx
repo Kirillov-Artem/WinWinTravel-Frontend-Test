c633e352ec69fad9a4bc5c6b0be5d7e0aa101412
Kirillov-Artem <artemkirillov2017@gmail.com>
2025-08-26 18:28:50 +0300

    feat: add confirmation modal with full styling

--- src/components/FilterModal.tsx
+++ src/components/FilterModal.tsx
@@ -7,9 +7,12 @@ import { FilterChooseOption } from '@api/types/Filter'
 import { FilterResponse, fetchFilters } from './../api'
 import CloseIcon from './../images/Modal/close.svg'
 import { useFilterStore } from './../store/filterStore'
+import ConfirmModal from './ConfirmModal'
+
+// import ApplyBtn from './applyBtn'
 
 const FilterModal = () => {
-	const { closeModal } = useFilterStore()
+	const { closeModal, openConfirm, isOpenConfrim } = useFilterStore()
 	const { t } = useTranslation()
 
 	const { data: filterData } = useQuery<FilterResponse>({
@@ -18,57 +21,63 @@ const FilterModal = () => {
 	})
 
 	return (
-		<div className="absolute top-0 right-0 left-0 z-50 flex items-center justify-center backdrop-blur-md py-[80px]">
-			<div className="relative w-full max-w-[1220px] bg-white rounded-2xl [box-shadow:0_0_15px_-3px_rgb(0_0_0_/_0.1),_0_0_6px_-4px_rgb(0_0_0_/_0.1)] flex flex-col pt-[32px] pb-[40px] px-[33px]">
-				<div className="flex items-center">
-					<h2 className="font-inter font-medium text-[40px] text-center flex-1">
-						{t('filter:filterTitle')}
-					</h2>
-					<button
-						onClick={closeModal}
-						className="w-[24px] h-[24px] cursor-pointer"
-					>
-						<CloseIcon />
-					</button>
-				</div>
-				<div className="w-full h-[2px] bg-[#B4B4B4] mt-[25px]"></div>
+		<>
+			<div className="fixed inset-0 z-40 bg-black/40 backdrop-blur-md" />
+			<div className="absolute top-0 right-0 left-0 z-40 flex items-center justify-center backdrop-blur-md py-[80px]">
+				<div className="relative w-full max-w-[1220px] bg-white rounded-2xl [box-shadow:0_0_15px_-3px_rgb(0_0_0_/_0.1),_0_0_6px_-4px_rgb(0_0_0_/_0.1)] flex flex-col pt-[32px] pb-[40px] px-[33px]">
+					<div className="flex items-center">
+						<h2 className="font-inter text-[40px] text-center flex-1">
+							{t('filter:filterTitle')}
+						</h2>
+						<button
+							onClick={closeModal}
+							className="w-[24px] h-[24px] cursor-pointer"
+						>
+							<CloseIcon />
+						</button>
+					</div>
+					<div className="w-full h-[2px] bg-[#B4B4B4] mt-[25px]"></div>
 
-				<div className="flex-1 overflow-y-auto pt-5 space-y-8">
-					{filterData?.filterItems.map(filter => (
-						<div key={filter.id}>
-							<h3 className="text-2xl font-medium text-[#31393C] mb-[24px]">
-								{t(`filter:filter.${filter.id}.name`)}
-							</h3>
-							<div className="grid grid-cols-3 gap-2">
-								{filter.options.map((option: FilterChooseOption) => (
-									<label
-										key={option.id}
-										className="flex items-center gap-2 cursor-pointer"
-									>
-										<input
-											type="checkbox"
-											className="mr-[16px] w-4 h-4 border-2 border-[#31393C] rounded accent-orange-500"
-										/>
-										{t(`filter:filter.${filter.id}.${option.id}`)}
-									</label>
-								))}
+					<div className="flex-1 overflow-y-auto pt-5 space-y-8">
+						{filterData?.filterItems.map(filter => (
+							<div key={filter.id}>
+								<h3 className="text-2xl font-medium text-[#31393C] mb-[24px]">
+									{t(`filter:filter.${filter.id}.name`)}
+								</h3>
+								<div className="grid grid-cols-3 gap-2">
+									{filter.options.map((option: FilterChooseOption) => (
+										<label
+											key={option.id}
+											className="flex items-center gap-2 cursor-pointer"
+										>
+											<input
+												type="checkbox"
+												className="mr-[16px] w-4 h-4 border-2 border-[#31393C] rounded accent-orange-500"
+											/>
+											{t(`filter:filter.${filter.id}.${option.id}`)}
+										</label>
+									))}
+								</div>
+								<div className="w-full h-[2px] bg-[#B4B4B4] mt-[25px]"></div>
 							</div>
-							<div className="w-full h-[2px] bg-[#B4B4B4] mt-[25px]"></div>
-						</div>
-					))}
-				</div>
+						))}
+					</div>
 
-				{/* Footer */}
-				<div className="flex items-center justify-center pt-[32px]">
-					<button className="cursor-pointer translate-x-[50%] mx-auto px-8 py-[18px] bg-[#FF5F00] text-white rounded-[16px] font-medium hover:bg-orange-600 transition grow max-w-[184px]">
-						{t('filter:apply')}
-					</button>
-					<button className="cursor-pointer text-base underline text-[#078691] hover:text-gray-700">
-						{t('filter:resetFilters')}
-					</button>
+					<div className="flex items-center justify-center pt-[32px]">
+						<button
+							onClick={openConfirm}
+							className="cursor-pointer translate-x-[50%] mx-auto px-8 py-[18px] bg-[#FF5F00] text-white rounded-[16px] font-medium hover:bg-orange-600 transition grow max-w-[184px]"
+						>
+							{t('filter:apply')}
+						</button>
+						<button className="cursor-pointer text-base underline text-[#078691] hover:text-gray-700">
+							{t('filter:resetFilters')}
+						</button>
+					</div>
 				</div>
+				{isOpenConfrim && <ConfirmModal />}
 			</div>
-		</div>
+		</>
 	)
 }
 
--- src/store/filterStore.ts
+++ src/store/filterStore.ts
@@ -2,14 +2,20 @@ import { create } from 'zustand'
 
 interface FilterStore {
 	isOpenModal: boolean
+	isOpenConfrim: boolean
 
 	openModal: () => void
 	closeModal: () => void
+	openConfirm: () => void
+	closeConfirm: () => void
 }
 
 export const useFilterStore = create<FilterStore>(set => ({
 	isOpenModal: false,
+	isOpenConfrim: false,
 
 	openModal: () => set({ isOpenModal: true }),
-	closeModal: () => set({ isOpenModal: false })
+	closeModal: () => set({ isOpenModal: false }),
+	openConfirm: () => set({ isOpenConfrim: true }),
+	closeConfirm: () => set({ isOpenConfrim: false })
 }))
